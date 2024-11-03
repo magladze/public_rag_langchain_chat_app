7736133810dab840efd285f824d53b439e903fdf
mike <mike@intellisoftventures.com>
2024-11-03 10:32:27 -0800

    adding markdown

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from PyPDF2 import PdfReader
 from InstructorEmbedding import INSTRUCTOR
 import sentence_transformers
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
+# from langchain.llms import HuggingFaceHub
 
 import os
 
